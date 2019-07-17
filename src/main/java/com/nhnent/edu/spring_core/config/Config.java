1d4fd9dbb8fafffc84cb9abd66b198d649133913
SambaLim <limsungho07@hanmail.net>
2019-07-17 15:23:06 +0900

    bean primary

--- src/main/java/com/nhnent/edu/spring_core/config/Config.java
+++ src/main/java/com/nhnent/edu/spring_core/config/Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import com.nhnent.edu.spring_core.service.PackageMarker;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.nhnent.edu.spring_core.component.MemberService;
 import com.nhnent.edu.spring_core.component.impl.MemberServiceImpl;
@@ -11,10 +12,11 @@ import com.nhnent.edu.spring_core.service.NotificationService;
 import com.nhnent.edu.spring_core.service.impl.KakaoServiceImpl;
 
 @Configuration
-@ComponentScan("com.nhnent.edu.spring_core.service")
+@ComponentScan("com.nhnent.edu.spring_core")
 public class Config {
 	
 	@Bean(initMethod="init", destroyMethod="destroy")
+	@Primary
 	public MemberService memberService() {
 		return new MemberServiceImpl();
 	}
